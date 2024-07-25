96c4e005e83f3b48f126777c377360d3050642f3
Shubham Kumar Gupta <skg2002007@gmail.com>
2024-07-25 23:02:23 +0530

    Update lms.sql

--- lms.sql
+++ lms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2020 at 10:42 AM
+-- Generation Time: May 08, 2024 at 10:42 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.26
 
